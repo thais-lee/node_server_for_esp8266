098017a9fd5272f6c182c85c9760d87c341bc3ad
thais-lee <meoconlonton2001@gmail.com>
2022-05-14 18:35:29 +0700

    add post api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import getApi from './routes/getData.route';
+import postApi from './routes/postData.route'
+
 export default () => {
   const app = Router();
   getApi(app);
+  postApi(app);
   return app;
 };
